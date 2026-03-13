d616bdbfe9729284e880c786b4d4e21bf5c02403
JecsanC <jecsanr7@gmail.com>
2026-03-12 19:20:23 -0600

    Add additional cliente and producto entries to data files

--- data/clientes.ts
+++ data/clientes.ts
@@ -8,5 +8,15 @@ export const clientes = [
     id: 2,
     nombre: "María López",
     email: "maria@gmail.com"
+  },
+  {
+    id: 3,
+    nombre: "Jecsan Cárdenas",
+    email: "jecsanr7@gmail.com"
+  },
+  {
+    id: 4,
+    nombre: "Enrique Comparan",
+    email: "19460205@colima.tecnm.mx"
   }
 ];
--- data/productos.ts
+++ data/productos.ts
@@ -8,5 +8,26 @@ export const productos = [
     id: 2,
     nombre: "Mouse",
     precio: 300
-  }
+  },
+    {
+    id: 3,
+    nombre: "Teclado",
+    precio: 1200
+  },
+    {
+    id: 4,
+    nombre: "Monitor",
+    precio: 3500
+  },
+    {
+    id: 5,
+    nombre: "Audifonos",
+    precio: 600
+  },
+    {
+    id: 6,
+    nombre: "Mousepad",
+    precio: 200
+  },
+  
 ];
